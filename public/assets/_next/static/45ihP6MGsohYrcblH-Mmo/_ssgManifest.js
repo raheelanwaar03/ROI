89fbb57cd0571447605efbf30a31638f5b7a2b9a
raheel0316 <raheelanwaar0316@gmail.com>
2024-02-24 01:08:06 -0800

    installation

---
+++ public/assets/_next/static/45ihP6MGsohYrcblH-Mmo/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout-us","\u002Fcommission-policy","\u002Fcontact-us","\u002Ffaq","\u002Fplans","\u002Fprivacy-policy","\u002Frefund-policy","\u002Fterms-and-conditions"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
